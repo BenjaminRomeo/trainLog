2645d4fae1b963f400177c02587cc4617015c54c
BenjaminRomeo <benjiplop@hotmail.fr>
2018-01-15 00:34:45 +0100

    SwipeOut

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,6 +1,6 @@
 {/* Common Component import */ }
-import React from "react";
-import { StatusBar, ScrollView } from "react-native"
+import React from "react"
+import { StatusBar, ScrollView, ListView, TouchableHighlight } from "react-native"
 import { Button, Text, Container, Body, List, Header, Title, Left, Icon, Right, View } from "native-base"
 import Swipeout from 'react-native-swipeout'
 
@@ -11,75 +11,107 @@ import styles from './styles'
   Class definition
   Home Screen => Training
 */}
+
+const rows = [
+    {
+        title: 'Push',
+        worked: 'Pectoraux',
+        day: 'Lundi'
+    },
+    {
+        title: 'Leg',
+        worked: 'Jambe',
+        day: 'Mardi'
+    },
+    {
+        title: 'Pull',
+        worked: 'Dos',
+        day: 'Mercredi'
+    },
+    {
+        title: 'Push',
+        worked: 'Pectoraux',
+        day: 'Jeudi'
+    },
+    {
+        title: 'Push',
+        worked: 'Dos',
+        day: 'Vendredi'
+    }
+]
+
 class HomeScreen extends React.Component {
     constructor(props) {
         super(props)
-     
+        let ds = new ListView.DataSource({rowHasChanged: (row1, row2) => true})
+
         this.state = ({
-            activeRow: null
+            swipeoutBtns: [
+                {
+                    text: 'Editer',
+                    backgroundColor: 'green',
+                    underlayColor: 'grey',
+                },
+                {
+                    text: 'Dupliquer',
+                    backgroundColor: 'blue',
+                    underlayColor: 'grey',
+                },
+                {
+                    text: 'Archiver',
+                    backgroundColor: 'orange',
+                    underlayColor: 'grey',
+                },
+                {
+                    text: 'Supprimer',
+                    backgroundColor: 'red',
+                    underlayColor: 'grey',
+                }
+            ],
+            dataSource: ds.cloneWithRows(rows),
+            sectionID: null,
+            rowID: null
         })
     }
+
+    _renderRow(rowData, sectionID, rowID) {
+        return (
+            <Swipeout
+                style={styles.swipeOut} 
+                sensitivity = {50}
+                right={this.state.swipeoutBtns}
+                rowID={rowID}
+                sectionID={sectionID}
+                autoClose={rowData.autoClose}
+                close={!rowData.active}
+                onOpen={(sectionID, rowID) => this._handleSwipeout(sectionID, rowID)}
+            >
+                <TouchableHighlight style={styles.swipeOut}>
+                    <View style={styles.swipeOutView}>
+                        <Text style={styles.swipeOutTitle}>{rowData.title}</Text>
+                        <Text style={styles.swipeOutWorked}>{rowData.worked}</Text>
+                        <Text style={styles.swipeOutDay}>{rowData.day}</Text>
+                    </View>
+                </TouchableHighlight>
+            </Swipeout>
+        ) 
+    }
+
+    _updateDataSource = function(data) {
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(data)
+        })
+    }
+
+    _handleSwipeout = function(sectionID, rowID) {
+        for (var i = 0; i < rows.length; i++) {
+          if (i != rowID) rows[i].active = false
+          else rows[i].active = true
+        }
+        this._updateDataSource(rows)
+      }
+
     render() {
-
-        let swipeoutBtns = [
-            {
-                text: 'Editer',
-                backgroundColor: 'green',
-                underlayColor: 'grey',
-                sensitivity: 10,
-                autoClose: true,
-            },
-            {
-                text: 'Dupliquer',
-                backgroundColor: 'blue',
-                underlayColor: 'grey',
-                sensitivity: 10,
-            },
-            {
-                text: 'Archiver',
-                backgroundColor: 'orange',
-                underlayColor: 'grey',
-                sensitivity: 10,
-            },
-            {
-                text: 'Supprimer',
-                backgroundColor: 'red',
-                underlayColor: 'grey',
-                sensitivity: 10,
-            }
-        ], workoutList = [
-            {
-                title: 'Push',
-                worked: 'Pectoraux',
-                day: 'Lundi',
-                rowId: 1
-            },
-            {
-                title: 'Leg',
-                worked: 'Jambe',
-                day: 'Mardi',
-                rowId: 2
-            },
-            {
-                title: 'Pull',
-                worked: 'Dos',
-                day: 'Mercredi',
-                rowId: 3
-            },
-            {
-                title: 'Push',
-                worked: 'Pectoraux',
-                day: 'Jeudi',
-                rowId: 4
-            },
-            {
-                title: 'Push',
-                worked: 'Dos',
-                day: 'Vendredi',
-                rowId: 5
-            },
-        ]
-
         return (
             <Container style={styles.container}>
                 <Header style={styles.header}>
@@ -100,23 +132,7 @@ class HomeScreen extends React.Component {
                         </Button>
                     </Right>
                 </Header>
-                <List
-                    dataArray={workoutList}
-                    renderRow={data => {
-                        return (
-                            <Swipeout style={styles.swipeOut} right={swipeoutBtns} onOpen={(secId, rowId, direction) => {
-                                this.setState({ activeRow: data.rowId })
-                                console.log(this.state.activeRow)
-                            }}>
-                                <View style={styles.swipeOutView}>
-                                    <Text style={styles.swipeOutTitle}>{data.title}</Text>
-                                    <Text style={styles.swipeOutWorked}>{data.worked}</Text>
-                                    <Text style={styles.swipeOutDay}>{data.day}</Text>
-                                </View>
-                            </Swipeout>
-                        )
-                    }}
-                />
+                <ListView dataSource={this.state.dataSource} renderRow={this._renderRow.bind(this)}/>
             </Container>
         )
     }
